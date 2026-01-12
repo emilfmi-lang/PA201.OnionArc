2937f20fda355258f6d4e3113d9a64f8bfd8bcf7
emilfmi.lang <emilfmi@code.edu.az>
2026-01-12 09:27:36 +0400

    added AccountController

---
+++ OnionArch/src/Presentation/OnionArch.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionArch.Application.Dtos.AppUser;
+using OnionArch.Application.Services.Interfaces;
+
+namespace OnionArch.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(IAccountService accountService) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserRegisterDto userRegister)
+    {
+        return Ok("Qeydiyyat ugurludur");
+    }
+}
--- OnionArch/src/Presentation/OnionArch.WebAPI/obj/Debug/net10.0/OnionArch.WebAPI.AssemblyInfo.cs
+++ OnionArch/src/Presentation/OnionArch.WebAPI/obj/Debug/net10.0/OnionArch.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnionArch.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2c2946d6a2d23f041e4c6efaeeaf95cd706940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee876869981f64aab66d9aa7afba5b5b26d4f324")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnionArch.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnionArch.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
