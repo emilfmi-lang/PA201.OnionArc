ee876869981f64aab66d9aa7afba5b5b26d4f324
emilfmi.lang <emilfmi@code.edu.az>
2026-01-12 09:21:29 +0400

    added UserLogin UserRegister Validator

---
+++ OnionArch/src/Core/OnionArch.Application/Validators/AccountValidator/AppUserLoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OnionArch.Application.Dtos.AppUser;
+
+namespace OnionArch.Application.Validators.AccountValidator;
+
+public class AppUserLoginValidator : AbstractValidator<UserLoginDto>
+{
+    public AppUserLoginValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Username və ya Email boş ola bilməz");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Şifrə boş ola bilməz")
+            .MinimumLength(6).WithMessage("Şifrə minimum 6 simvol olmalıdır");
+    }
+}
--- OnionArch/src/Core/OnionArch.Application/obj/Debug/net10.0/OnionArch.Application.AssemblyInfo.cs
+++ OnionArch/src/Core/OnionArch.Application/obj/Debug/net10.0/OnionArch.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnionArch.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8baf96ae6a683f35991ac99f7fa4087d9869232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bce15147f00d8071f76cd6366982192dc82066d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnionArch.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnionArch.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
